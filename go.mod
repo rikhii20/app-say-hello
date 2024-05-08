e5c938f05e230551e021bb1041f71a0df9a75873
RIKHI SOBARI <RIKHI.SOBARI@btpn.com>
2024-05-08 11:30:17 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rikhii20/app-say-hello
+
+go 1.21.4
+
+require github.com/rikhii20/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/rikhii20/go-say-hello/v2"
+)
+
+func main() {
+	name := "Rikhi"
+	fmt.Println(go_say_hello.SayHello(name))
+}
